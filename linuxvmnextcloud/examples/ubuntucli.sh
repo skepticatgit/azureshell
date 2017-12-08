e7dccc8f2d7000c1eac2d0d93af155ef911c35a6
Andrei Fateev <andf@microsoft.com>
2017-12-08 14:03:14 -0500

    Added bash scripts for Ubuntu VM

--- linuxvmnextcloud/examples/ubuntucli.sh
+++ linuxvmnextcloud/examples/ubuntucli.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 myResourceGroup=<setItHere>
 myPublicDNS=<setItHere>
+dnsSuffix=".cloudapp.azure.com"
 myUser=<setItHere>
 myPass=<setItHere>
 vmSize=Standard_D1_v2
 
+echo "STEP 1 of 8: Creating the resource group, please wait."
 az group create --name $myResourceGroup --location eastus2
 
+echo "STEP 2 of 8: Creating virtual network, please wait."
 az network vnet create \
     --resource-group $myResourceGroup \
     --name myVnet \
@@ -14,15 +17,18 @@ az network vnet create \
     --subnet-name mySubnet \
     --subnet-prefix 192.168.1.0/24
 
+echo "STEP 3 of 8: Creating Public IP, please wait."
 az network public-ip create \
     --resource-group $myResourceGroup \
     --name myPublicIP \
     --dns-name $myPublicDNS
 
+echo "STEP 4 of 8: Creating Network Security Group, please wait."
 az network nsg create \
     --resource-group $myResourceGroup \
     --name myNetworkSecurityGroup
 
+echo "STEP 5 of 8: Creating inbound firewall rule, please wait."
 az network nsg rule create \
     --resource-group $myResourceGroup \
     --nsg-name myNetworkSecurityGroup \
@@ -32,6 +38,7 @@ az network nsg rule create \
     --destination-port-range 22 \
     --access allow
 
+echo "STEP 6 of 8: Creating inbound firewall rule, please wait."
 az network nsg rule create \
     --resource-group $myResourceGroup \
     --nsg-name myNetworkSecurityGroup \
@@ -41,6 +48,7 @@ az network nsg rule create \
     --destination-port-range 443 \
     --access allow
 
+echo "STEP 7 of 8: Creating network interface card, please wait."
 az network nic create \
     --resource-group $myResourceGroup \
     --name myNic \
@@ -49,6 +57,7 @@ az network nic create \
     --public-ip-address myPublicIP \
     --network-security-group myNetworkSecurityGroup
 
+echo "STEP 8 of 8: Creating virtual machine, please wait."
 az vm create \
     --resource-group $myResourceGroup \
     --name myVM \
@@ -59,3 +68,6 @@ az vm create \
     --admin-username $myUser \
     --admin-password $myPass \
     --size $vmSize
+
+echo "Your VM has been successfully set-up. You can SSH to it via DNS name below:"
+echo $myPublicDNS$dnsSuffix
